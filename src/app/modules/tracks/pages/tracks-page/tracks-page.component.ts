78f2bd9ea37df5edc699fe9b410f3f8a9245b965
jhoandvid <rojasalmariojhoandvid@gmail.com>
2022-01-25 19:49:14 -0500

    version 3.1

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
 export class TracksPageComponent implements OnInit {
+mockTracksList=[
+  {
+    name:'BEBE (Oficial)'
+  },
+  
+  {
+    name:'BEBE (Oficial)'
+  },
 
+  {
+    name:'BEBE (Oficial)'
+  }
+
+]
   constructor() { }
 
   ngOnInit(): void {
